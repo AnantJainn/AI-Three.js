9971c970b4aaf2564c06f6169395aaabbbfc699f
Anant Jain <ritujainanant2810@gmail.com>
2024-03-02 22:53:37 +0530

    UI changed

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
 
-const Tab = () => {
+  const activeStyles = isFilterTab && isActiveTab ? { backgroundColor: snap.color, opacity: 0.5 } : { backgroundColor: "transparent", opacity: 1 }
   return (
-    <div>Tab</div>
+    <div
+    key={tab.name}
+    className={`tab-btn ${isFilterTab ? 'rounded-full glassmorhism' : 'rounded-4'}`}>Tab</div>
   )
 }
 
